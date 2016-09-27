7df3866679fa898e95f805c523cefb1ccbfb7179
Paul Marshall <paul.marshall@wavecrestsoftware.co.uk>
2016-09-27 20:12:08 +0100

    Added more tests

--- tests/MeterSpec.js
+++ tests/MeterSpec.js
@@ -38,8 +38,56 @@ describe("Meter", function() {
     digits[2].value("3");
     digits[3].value("4");
     digits[4].value("5");
-    digits[4].value("6");
+    digits[5].value("6");
 
     expect(dial.reading()).toBe("12345.6");
   });      
+
+  it("should indicate an invalid entry", function() {
+    var meter = new Meter(5,0,1);
+
+    var dial = meter.dials()[0];
+    var digits = dial.digits();
+
+    digits[0].value("1");
+    digits[1].value("2");
+    digits[2].value("3");
+    digits[3].value("4");
+    digits[4].value("a");
+
+    expect(dial.reading()).toBe("1234x");
+  });
+
+  it("should indicate an incomplete reading", function() {
+    var meter = new Meter(5,0,1);
+
+    var dial = meter.dials()[0];
+    var digits = dial.digits();
+
+    digits[0].value("1");
+    digits[1].value("2");
+    digits[2].value("3");
+    digits[3].value("4");
+
+    expect(dial.reading()).toBe("1234x");
+  });  
+
+  it("should indicate that a dial digit is within the integer part of the reading", function() {
+    var meter = new Meter(1,0,1);
+
+    var dial = meter.dials()[0];
+    var digits = dial.digits();
+
+    expect(digits[0].css()).toBe("left");
+  });
+
+  it("should indicate that a dial digit is within the fractional part of the reading", function() {
+    var meter = new Meter(1,1,1);
+
+    var dial = meter.dials()[0];
+    var digits = dial.digits();
+
+    expect(digits[1].css()).toBe("right");
+  });  
+
 });
